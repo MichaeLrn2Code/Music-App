b937a8b2ccecc4d4fb542799db48d51d02753765
MichaeLrn2Code <120570867+MichaeLrn2Code@users.noreply.github.com>
2023-05-26 22:44:27 -0400

    Run npm delpoy

---
+++ .next/static/X_KveC9vD0cQ__Kj2hNZL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/647-8e807633823ae763.js","static/chunks/pages/index-6fca6804837e3c80.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
